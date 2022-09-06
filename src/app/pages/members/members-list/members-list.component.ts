ad9e7043589608c327bd5fe6d89851ac6dd16abb
Oguzhan19 <oguzhancart1@gmail.com>
2022-09-06 16:32:17 +0300

    continuing education on demo examples

--- src/app/pages/members/members-list/members-list.component.ts
+++ src/app/pages/members/members-list/members-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-members-list',
   templateUrl: './members-list.component.html',
-  styleUrls: ['./members-list.component.css']
+  styleUrls: ['./members-list.component.scss']
 })
 export class MembersListComponent implements OnInit {
 
